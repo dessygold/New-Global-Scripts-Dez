4d13273146b3bc3413a43607252a323f4ff56243
Desmond Osatuyi <desmond.osatuyi@catapultsystems.com>
2022-03-29 17:32:36 -0400

    new

--- ResourceHealth_State/ResourceAvailabilityWorking.ps1
+++ ResourceHealth_State/ResourceAvailabilityWorking.ps1
@@ -25,45 +25,10 @@
         v1.0
         Desmond Osatuyi 2022
     #>
-#This function generate auth token using REST api  
-## Variables
- $apiEndpointUri = "https://management.azure.com/"  
- $tenantId = "e10ddeb6-af98-4762-a6a6-a6caf23f2ba1"  
- $applicationId = "45b19b88-611d-4166-b345-66ff89c25403"  
- $secret = "lx44Wb5yqTjq14DuUi3R_.bMyb3EcZozuo" 
-Function GetAuthTokenInvokingRestApi {  
-    Param(  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [String]$tenantId,  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [String]$applicationId,  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [String]$secret,  
-       [Parameter(Mandatory)][ValidateNotNull()][ValidateNotNullOrEmpty()]  
-       [string]$apiEndpointUri  
- )  
- $encodedSecret = [System.Web.HttpUtility]::UrlEncode($secret)  
- $RequestAccessTokenUri = "https://login.microsoftonline.com/$tenantId/oauth2/token"  
- $body = "grant_type=client_credentials&client_id=$applicationId&client_secret=$encodedSecret&resource=$apiEndpointUri"  
- $contentType = 'application/x-www-form-urlencoded'  
- try {  
-    $authToken = Invoke-RestMethod -Method Post -Uri $RequestAccessTokenUri -Body $body -ContentType $contentType  
-    Write-Output $authToken 
-    }  
-    catch { throw }  
- }  
-#  $apiEndpointUri = "https://management.azure.com/"  
-#  $tenantId = "e10ddeb6-af98-4762-a6a6-a6caf23f2ba1"  
-#  $applicationId = "45b19b88-611d-4166-b345-66ff89c25403"  
-#  $secret = "lx44Wb5yqTjq14DuUi3R_.bMyb3EcZozuo"  
- #$authToken = GetAuthTokenInvokingRestApi -apiEndpointUri $apiEndpointUri -tenantId $tenantId -applicationId $applicationId -secret $secret  
- #if (-   not $authToken) { throw "One of the provided login information is invalid 'tenantId: $tenantId', 'applicationId: $applicationId', 'secret: $secret' " }  
- Write-Host "Auth token by GetAuthTokenInvokingRestApi :"  
- Write-Host $authToken -ForegroundColor Yellow  
- #When we run above powerhsell script we can get auth tokens as below      
+    
 ###################################  Get Azure AD application token FROM POST MAN #####################################################
 
-#$authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tLyIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2UxMGRkZWI2LWFmOTgtNDc2Mi1hNmE2LWE2Y2FmMjNmMmJhMS8iLCJpYXQiOjE2NDg1ODc4ODMsIm5iZiI6MTY0ODU4Nzg4MywiZXhwIjoxNjQ4NTkxNzgzLCJhaW8iOiJFMlpnWUpnWXNmQnE5VDJyVlc3WHU4Ui8zTnFnQlFBPSIsImFwcGlkIjoiNDViMTliODgtNjExZC00MTY2LWIzNDUtNjZmZjg5YzI1NDAzIiwiYXBwaWRhY3IiOiIxIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTEwZGRlYjYtYWY5OC00NzYyLWE2YTYtYTZjYWYyM2YyYmExLyIsImlkdHlwIjoiYXBwIiwib2lkIjoiMGFjZGM5OTQtMTc3Yi00MWUwLWIxNDAtYWUwYjIxODZhZDQwIiwicmgiOiIwLkFVWUF0dDRONFppdllrZW1wcWJLOGo4cm9VWklmM2tBdXRkUHVrUGF3ZmoyTUJPQUFBQS4iLCJzdWIiOiIwYWNkYzk5NC0xNzdiLTQxZTAtYjE0MC1hZTBiMjE4NmFkNDAiLCJ0aWQiOiJlMTBkZGViNi1hZjk4LTQ3NjItYTZhNi1hNmNhZjIzZjJiYTEiLCJ1dGkiOiJzYk1LYlBCOE8wcTM3WlR0VktaVEFBIiwidmVyIjoiMS4wIiwieG1zX3RjZHQiOjE2NDE4Njc5NzJ9.KicIfZQouccR3yKaf0Pohwdkf7zVpPvkS44-XFod54xtZ8GBxgWh6adSoRS9gQP1u_CyX3gPmvt92ckGoVk0HQYVUscdtm7-DLUxr657u57G4UMw0zqUoEuB0_wgm9XZmGvpAs_xQIjg5VmQboswHADJc6msjR_htJXcAZLA4E5pAJ_Mb0y9tseEy5r7Lqx2jQ79MyMH3sFbtMdlfu6loKWXoV8u1nd-XnAbYC-EcApQphRHhEOLkxdwW6WQo0prMi42EFXiOwFSv9o0fzFZYDbYB519aX5jBxB3ZYjcjKe-q5E1io6W8WNJQfgeqh0ELzJV4hChzQy43u-kQN1E1w"
+$authToken = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyIsImtpZCI6ImpTMVhvMU9XRGpfNTJ2YndHTmd2UU8yVnpNYyJ9.eyJhdWQiOiJodHRwczovL21hbmFnZW1lbnQuYXp1cmUuY29tLyIsImlzcyI6Imh0dHBzOi8vc3RzLndpbmRvd3MubmV0L2UxMGRkZWI2LWFmOTgtNDc2Mi1hNmE2LWE2Y2FmMjNmMmJhMS8iLCJpYXQiOjE2NDg1ODc4ODMsIm5iZiI6MTY0ODU4Nzg4MywiZXhwIjoxNjQ4NTkxNzgzLCJhaW8iOiJFMlpnWUpnWXNmQnE5VDJyVlc3WHU4Ui8zTnFnQlFBPSIsImFwcGlkIjoiNDViMTliODgtNjExZC00MTY2LWIzNDUtNjZmZjg5YzI1NDAzIiwiYXBwaWRhY3IiOiIxIiwiaWRwIjoiaHR0cHM6Ly9zdHMud2luZG93cy5uZXQvZTEwZGRlYjYtYWY5OC00NzYyLWE2YTYtYTZjYWYyM2YyYmExLyIsImlkdHlwIjoiYXBwIiwib2lkIjoiMGFjZGM5OTQtMTc3Yi00MWUwLWIxNDAtYWUwYjIxODZhZDQwIiwicmgiOiIwLkFVWUF0dDRONFppdllrZW1wcWJLOGo4cm9VWklmM2tBdXRkUHVrUGF3ZmoyTUJPQUFBQS4iLCJzdWIiOiIwYWNkYzk5NC0xNzdiLTQxZTAtYjE0MC1hZTBiMjE4NmFkNDAiLCJ0aWQiOiJlMTBkZGViNi1hZjk4LTQ3NjItYTZhNi1hNmNhZjIzZjJiYTEiLCJ1dGkiOiJzYk1LYlBCOE8wcTM3WlR0VktaVEFBIiwidmVyIjoiMS4wIiwieG1zX3RjZHQiOjE2NDE4Njc5NzJ9.KicIfZQouccR3yKaf0Pohwdkf7zVpPvkS44-XFod54xtZ8GBxgWh6adSoRS9gQP1u_CyX3gPmvt92ckGoVk0HQYVUscdtm7-DLUxr657u57G4UMw0zqUoEuB0_wgm9XZmGvpAs_xQIjg5VmQboswHADJc6msjR_htJXcAZLA4E5pAJ_Mb0y9tseEy5r7Lqx2jQ79MyMH3sFbtMdlfu6loKWXoV8u1nd-XnAbYC-EcApQphRHhEOLkxdwW6WQo0prMi42EFXiOwFSv9o0fzFZYDbYB519aX5jBxB3ZYjcjKe-q5E1io6W8WNJQfgeqh0ELzJV4hChzQy43u-kQN1E1w"
 
 ######################## Create Auth Header with the Generated Auth Token###########################################################
 
---
+++ ResourceHealth_State/test.ps1
@@ -0,0 +1,31 @@
+#####################################################################################################
+$TenantId = "e10ddeb6-af98-4762-a6a6-a6caf23f2ba1" 
+#$TenantId = "desmondosatuyiyahoo.onmicrosoft.com"
+$ClientID = "45b19b88-611d-4166-b345-66ff89c25403" 
+$ClientSecret = "lx44Wb5yqTjq14DuUi3R_.bMyb3EcZozuo"
+
+## Variables
+# $apiEndpointUri = "https://management.azure.com/"  
+# $tenantId = "e10ddeb6-af98-4762-a6a6-a6caf23f2ba1"  
+# $applicationId = "45b19b88-611d-4166-b345-66ff89c25403"  
+# $secret = "lx44Wb5yqTjq14DuUi3R_.bMyb3EcZozuo" 
+
+## We can get an AAD access token for REST API calls using AzureAD Module.
+
+
+Install-Module AzureAD -Force
+Import-Module -Name "AzureAD"
+$AadModule = Get-Module -Name "AzureAD" -ListAvailable
+$adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+[System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
+
+$authority = "https://login.windows.net/$TenantId"
+$authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+$AdUserCred = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.ClientCredential" -ArgumentList $ClientID, $ClientSecret
+$token = ($authContext.AcquireTokenAsync("https://management.azure.com/", $AdUserCred)).Result.AccessToken
+Write-Output $token
+
+$authHeader = @{
+    "Content-Type" = "application/json"
+    "Authorization"= "Bearer " + $token
+    }
