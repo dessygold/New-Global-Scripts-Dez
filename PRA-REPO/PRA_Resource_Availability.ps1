d1d68cf421f0110cf4e5a7063a5a85edf4ec678f
Desmond Osatuyi <desmond.osatuyi@catapultsystems.com>
2022-04-05 13:20:57 -0400

    added pra resource health script

---
+++ PRA-REPO/PRA_Resource_Availability.ps1
@@ -0,0 +1,125 @@
+<#
+    .SYNOPSIS
+       This let’s us get health state (availability status) on all Azure resources looping through all subscriptions and resource group.
+    .DESCRIPTION
+       Resource health helps you diagnose and get support when an Azure issue impacts your resources.
+       It informs you about the current and past health of your resources and helps you mitigate issues.
+       Resource health provides technical support when you need help with Azure service issues.
+
+    .PARAMETER ClientID
+        Azure AD application ID
+
+    .PARAMETER ClientSecret
+        Azure AD application secret.
+
+    .PARAMETER TenantId
+        Azure tenant ID
+      
+    .PARAMETER AuthToken
+       Generate an authentication token to use against Azure management Rest API’s
+          
+    .PARAMETER ResourceName
+        List/Name of all resources in Azure subscription
+                 
+    .NOTES
+        v1.0
+        Desmond Osatuyi 2022
+    #>
+#This function generate auth token using REST api  
+
+###################################################################################################################
+################################ Universal Variables For this Deployment ##########################################
+$kvName          = "kv-it-inf-aad-002"
+$spDisplayName   = "pra-resourse-health"
+$spkvSecretName  = "pra-resource-health"
+$TenantId        = "60a5b34b-c976-4591-a955-f65531bb4d4b" 
+$subscriptionID  = "50664444-1a76-4154-b346-a3702c8b7b47"
+$environment     = "AzureCloud"
+
+##################################### Login To Azure Subscription ##################################################
+Clear-AzContext -Force
+Set-AzContext -Tenant $TenantId -Subscription $subscriptionID
+Write-Host  "checking context ................."
+Connect-AzAccount -Environment $environment -TenantId $TenantId -Subscription $subscriptionID
+
+######################### Get App ID and Secret #####################################################################
+$ClientID     = (Get-AzADApplication -DisplayName $spDisplayName).AppId
+$ClientSecret = Get-AzKeyVaultSecret -VaultName $kvName -Name $spkvSecretName -AsPlainText
+
+$ClientID
+
+############### Generate an authentication token to use against Azure management Rest API’s ##############################
+$TokenEndpoint = {https://login.windows.net/{0}/oauth2/token} -f $TenantId 
+$resourceURL = "https://management.core.windows.net/";
+
+$Body = @{
+        'resource'= $resourceURL
+        'client_id' = $ClientID
+        'grant_type' = 'client_credentials'
+        'client_secret' = $ClientSecret
+}
+
+$params = @{
+    ContentType = 'application/x-www-form-urlencoded'
+    Headers = @{'accept'='application/json'}
+    Body = $Body
+    Method = 'Post'
+    URI = $TokenEndpoint
+}
+
+$authToken = Invoke-RestMethod @params
+
+
+######################## Create Auth Header with the Generated Auth Token ###########################################################
+
+$authHeader = @{
+    "Content-Type" = "application/json"
+    "Authorization"= "Bearer " + $authToken.access_token
+    }
+
+ ##########################  Loop through All Azure Subscription and get all resources using Graph API #######################
+############################# Add everything to a hash table ###############################################################
+$Subs = Get-AzSubscription
+$allHealths = @()
+foreach ($Sub in $Subs) {
+    Set-AzContext $Sub.id | Out-Null
+    Write-Host "Processing Subscription:" $($Sub).name
+
+    $APIVersion = "2022-01-01"
+    $RGURI = "https://management.azure.com/subscriptions/$Sub/resourcegroups?api-version=$APIVersion"
+
+    $ResourceGroups = (Invoke-RestMethod –Uri $RGURI –Method GET –Headers $authHeader).value.name
+
+    $resources = @{} ## add all Resources in a particular resource group
+    # Get all Azure resources for current subscription
+    foreach ($rg in $ResourceGroups) {
+        $ResourceGroupUri = "https://management.azure.com/subscriptions/$Sub/resourceGroups/$rg/resources?api-version=$APIVersion"
+        $res = (Invoke-RestMethod –Uri $ResourceGroupUri –Method GET –Headers $authHeader).value
+
+        #Create array of all resources
+        
+        $resources.Add($rg, $res)
+        ####################################################################  Resource Health #################
+        
+        #Set resource group name and use it in our url
+        $health = Invoke-RestMethod –Uri "https://management.azure.com/subscriptions/$Sub/resourceGroups/$rg/Providers/Microsoft.ResourceHealth/availabilityStatuses?api-version=2015-01-01" –Method GET –Headers $authHeader
+
+        $healths = $health.value | 
+            Select-Object `
+            @{n='subscriptionId';e={$_.id.Split("/")[2]}},
+            location,
+            @{n='resourceGroup';e={$_.id.Split("/")[4]}},
+            @{n='resource';e={$_.id.Split("/")[8]}},
+            @{n='status';e={$_.properties.availabilityState}}, # ie., Available or Unavailable
+            @{n='summary';e={$_.properties.summary}},
+            @{n='reason';e={$_.properties.reasonType}},
+            @{n='occuredTime';e={$_.properties.occuredTime}},
+            @{n='reportedTime';e={$_.properties.reportedTime}}
+        
+        foreach ($health in $healths) { 
+            $allHealths += $health
+        }
+    }
+ }       
+##################################  Export Result to CSV File ############################################
+$allHealths | Export-Csv -Force -Path ".\PRA_ResourceHealth-$(get-date -f yyyy-MM-dd-HHmm).csv"
