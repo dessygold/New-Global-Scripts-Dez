644e1f2ac48880434d80be923beb2443827437d4
Desmond Osatuyi <desmond.osatuyi@catapultsystems.com>
2022-05-19 12:09:50 -0400

    new

--- HarBourVest-Repo/AppRegExpiry.ps1
+++ HarBourVest-Repo/AppRegExpiry.ps1
@@ -1,19 +1,29 @@
-[CmdletBinding()]
-param(
-  $subscriptionId,
-  $subscriptionName,
-  $skip
-)
+$TenantId = "60a5b34b-c976-4591-a955-f65531bb4d4b" 
+$subscriptionID = "3ed787a6-c475-45af-be22-058195388d44"
+$environment    = "AzureCloud"
+
+##################################### Login To Azure Subscription ##################################################
+Clear-AzContext -Force
+Set-AzContext -Tenant $TenantId -Subscription $subscriptionID
+Write-Host  "checking context ................."
+Connect-AzAccount -Environment $environment -TenantId $TenantId -Subscription $subscriptionID
+
+# [CmdletBinding()]
+# param(
+#   $subscriptionId,
+#   $subscriptionName,
+#   $skip
+# )
 
 $reportPath = "./quarterly-review/QRreports/new_App-registrations.csv"
 $reportPath2 = "./quarterly-review/QRreports/expired_App-registrations.csv"
 $reportPath3 = "./quarterly-review/QRreports/stale_App-registrations.csv"
 
 function UpdateCSV($item) {
-    Add-Content -Path $reportPath -Value "$($item.SubscriptionName),$($item.DisplayName),$($item.ObjectId),$($item.Type),$($item.Created),$($item.EndDate)"
+    Add-Content -Force -Path $reportPath -Value "$($item.SubscriptionName),$($item.DisplayName),$($item.ObjectId),$($item.Type),$($item.Created),$($item.EndDate)"
 }
 function UpdateCSV2($item2) {
-    Add-Content -Path $reportPath2 -Value "$($item2.SubscriptionName),$($item2.DisplayName),$($item2.ObjectId),$($item2.Type),$($item2.Created),$($item2.EndDate)"
+    Add-Content -Force -Path $reportPath2 -Value "$($item2.SubscriptionName),$($item2.DisplayName),$($item2.ObjectId),$($item2.Type),$($item2.Created),$($item2.EndDate)"
 }
 function UpdateCSV3($item3) {
     Add-Content -Path $reportPath3 -Value "$($item3.SubscriptionName),$($item3.DisplayName),$($item3.ObjectId),$($item3.Type),$($item3.Created),$($item3.EndDate)"
@@ -27,10 +37,10 @@ $adApps = @()
 $adApps = Get-AzADApplication
 foreach ($adApp in $adApps){
     $appCredentials = @()
-    $appCredentials = Get-AzADAppCredential -ObjectId $adApp.ObjectId 
+    $appCredentials = Get-AzADAppCredential -ObjectId $adApp.Id 
     If($appCredentials){
         foreach($appCredential in $appCredentials | Where-Object { $_.EndDateTime -ge $date2}){
-            Write-Output "$($adApp.DisplayName) $($adApp.ObjectId) $($appCredential.Type) created on $($appCredential.StartDate) expires on $($appCredential.EndDate)"
+            Write-Output "$($adApp.DisplayName) $($adApp.ObjectId) $($appCredential.Type) created on $($appCredential.StartDateTime) expires on $($appCredential.EndDateTime)"
             updateCSV @{
                 "SubscriptionName" = $sub
                 "DisplayName" = $adApp.DisplayName
@@ -41,11 +51,11 @@ foreach ($adApp in $adApps){
             }
         }
         foreach($appCredential in $appCredentials | Where-Object { $_.EndDateTime -le $date}){
-            Write-Output "$($adApp.DisplayName) $($adApp.Id) $($appCredential.Type) created on $($appCredential.StartDate) expires on $($appCredential.EndDate)"
+            Write-Output "$($adApp.DisplayName) $($adApp.Id) $($appCredential.Type) created on $($appCredential.StartDateTime) expires on $($appCredential.EndDateTime)"
             updateCSV2 @{
                 "SubscriptionName" = $sub
                 "DisplayName" = $adApp.DisplayName
-                "ObjectId" = $adApp.ObjectId
+                "ObjectId" = $adApp.Id
                 "Type" = $appCredential.Type
                 "Created" = $appCredential.StartDateTime
                 "EndDate" = $appCredential.EndDateTime
@@ -57,7 +67,7 @@ foreach ($adApp in $adApps){
         updateCSV3 @{
             "SubscriptionName" = $sub
             "DisplayName" = $adApp.DisplayName
-            "ObjectId" = $adApp.ObjectId
+            "ObjectId" = $adApp.Id
             "Type" = "No Cert or Secrets Found"
             "Created" = "None"
             "EndDate" = "None"
